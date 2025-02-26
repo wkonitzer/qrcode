54bb7d7d24eda00d4c2a74774db7b5530d660a1d
William Konitzer <william@konitzer.co.uk>
2025-02-26 09:59:34 -0600

    second commit

---
+++ qrcode.py
@@ -0,0 +1,16 @@
+import qrcode
+
+# Replace with your website URL
+url = "https://moonlighteclipse.party"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("website_qr.png")
+
